07a0db9865cff3b4cf3e9a5ac750c7275ee170c2
BelovaNI <belova.ni@inbox.ru>
2023-02-20 15:49:09 +0500

    20/02/2023

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -1,8 +1,13 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import web.model.Car;
+import web.service.CarService;
+
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,12 +15,23 @@ import java.util.List;
 @Controller
 public class CarsController {
 
-    @GetMapping(value = "/cars")
-    public String printTable(ModelMap model) {
+    private CarService carService;
+    @Autowired
+    public CarsController() {}
 
-//        подключись к сервису с контроллера, создай ручку которая его будет вызывать с параметром
-//        сделай ручку GET /fillCarts пускай принимает число и заполняет указанным количеством машин таблицу
+    public CarsController(CarService carService) {
+        this.carService = carService;
+    }
 
+    public Car getCars(int count) {
+        Car answer = (Car) carService.getListByCount(count);
+        return answer;
+    }
+    @GetMapping(value = "/cars")//        подключись к сервису с контроллера, создай ручку которая его будет вызывать с параметром
+    public String printTable(Integer count, ModelMap model) { //        сделай ручку GET /fillCarts пускай принимает число и заполняет указанным количеством машин таблицу
+        List<Car> table = new ArrayList<>();
+        table.add(new CarsController().getCars(count));
+        model.addAttribute("table", table);
         return "cars";
     }
 }
--- src/main/java/web/dao/CarDaoImp.java
+++ src/main/java/web/dao/CarDaoImp.java
@@ -2,15 +2,23 @@ package web.dao;
 
 import org.springframework.stereotype.Repository;
 import web.model.Car;
-
+import java.util.ArrayList;
 import java.util.List;
 @Repository
 public class CarDaoImp implements CarDao{
     @Override
-    public List<Car> getListByCount(int count) {
-        /*
-        Метод, производящий выборку машин
-        */
-        return null;
+    public List<Car> getListByCount(int count) { // Метод, производящий выборку машин
+        List<Car> list = new ArrayList<>();
+        list.add(new Car(1922, "Cabriolet", "J-8"));
+        list.add(new Car(2005, "Lada", "Granta"));
+        list.add(new Car(2022, "Toyota", "Suprim"));
+        list.add(new Car(2050, "Nissan", "Future"));
+        list.add(new Car(3050, "Element", "5"));
+        if (list.isEmpty()) {
+            System.out.println("Пустая коллекция");
+        }else {
+            System.out.println(list.subList(0, count));
+        }
+        return list.subList(0, count);
     }
 }
